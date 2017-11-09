4e3775c438680fc046e0b28f6cac5859edf894c6
coldhans <33099639+coldhans@users.noreply.github.com>
2017-11-08 21:29:19 -0500

    Update and rename lineage.mk to dot.mk

--- dot.mk
+++ dot.mk
@@ -15,9 +15,9 @@
 $(call inherit-product, device/huawei/kiwi/full_kiwi.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_kiwi
+PRODUCT_NAME := dot_kiwi
 BOARD_VENDOR := huawei
 
 PRODUCT_GMS_CLIENTID_BASE := android-huawei
